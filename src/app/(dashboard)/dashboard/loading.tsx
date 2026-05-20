ec7065651e2d804114804c1e2f0c576cc85713cf
Xdiad47 <mail2diadem@gmail.com>
2026-05-20 11:15:57 +0530

    refactor: move frontend into dedicated frontend repository pointer

--- src/app/(dashboard)/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from '@/components/shared/LoadingSpinner'
-
-export default function DashboardLoading() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <LoadingSpinner size="md" />
-    </div>
-  )
-}
